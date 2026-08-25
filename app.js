271d213764fd780784032916e169b9631ce33073
DomRadomski <domradomski123@gmail.com>
2026-08-25 14:32:18 +0100

    setup

---
+++ app.js
@@ -0,0 +1,31 @@
+/////// app.js
+
+const path = require("node:path");
+const { Pool } = require("pg");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+
+const { Pool } = require("pg");
+
+module.exports = new Pool({
+  connectionString: 'postgres://domradomski@localhost:3000/first_auth',
+});
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(3000, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log("app listening on port 3000!");
+});
